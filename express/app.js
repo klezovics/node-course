087d6c143d9b58a55b4293e2615c4c666508d0ad
aklezovics <arturs.klezovics@raisin.com>
2020-03-27 22:28:44 +0100

    Add express.js server

---
+++ express/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const request = require('request')
+const app = express()
+const port = 3000
+
+app.get('/', ({query}, res) => {
+
+    let user = query.user;
+    let responseData = {greeting: "Hello world"}
+    if(user) {
+        responseData.user = user;
+    }
+
+    res.send(responseData)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
